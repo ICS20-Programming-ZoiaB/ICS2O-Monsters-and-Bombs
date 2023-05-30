ef846d063df3e4fc0edfbd1c5dc93ef745e38af4
ZoiaBaran <124380748+ZoiaBaran@users.noreply.github.com>
2023-05-30 14:55:32 +0000

    Added comments to files

--- js/game.js
+++ js/game.js
@@ -1,10 +1,29 @@
 /* global Phaser */
 
+// Copyright (c) 2023 ZoiaB All rights reserved
+//
+// Created by: ZoiaB
+// Created on: May 2023
+// This file contains the JS functions for index.html
+
+//* Game scene */
 const  config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: false
+    }
+  },
+  // Set background color
+  backgroundColor: 0x3f6e9a
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // We place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
 }
 
 const game = new Phaser.Game(config)
